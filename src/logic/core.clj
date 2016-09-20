1424869350b70db74f41ac801619e621e0a19ceb
Laszlo ASZALOS <laszalos@unideb.hu>
2016-09-20 21:15:11 +0200

    test: members of a given set

--- src/logic/core.clj
+++ src/logic/core.clj
@@ -25,33 +25,33 @@
     [:shownumcorrect]))
 
 (defn matching-questions   "generate questions"
-  [id question answers feedback-text feedback-files]
+  [id question answers feedback-text]
   (html [:question {:type "matching"}
          [:name [:text id]]
          [:questiontext
           {:format "html"} [:text (cdata question)]]
          [:generalfeedback {:format "html"}
-          [:text (cdata feedback-text) ]
-          feedback-files
-          ]
+          [:text (cdata feedback-text)]]
          (common-part)
          (for [cnt (range 0 (count answers)) 
                :let [v (nth answers cnt)]]
            [:subquestion {:format "html"} 
             [:text (cdata "\\(" v "\\)")]  ; an expression for MathJax!
-           [:answer [:text (str (inc cnt))]]])
-         ]))
+            [:answer [:text (str (inc cnt))]]])]))
+         
 
-(defn multi "generate MCQ tests" [id question answers]
+(defn multi "generate MCQ tests" [id question feedback answers]
   (html [:question {:type "multichoice"}
          [:name [:text id]]
          [:questiontext {:format "html"} [:text (cdata question)]]
-         [:generalfeedback {:format "html"} [:text] ]
+         [:generalfeedback {:format "html"} [:text (cdata feedback)]]
+         [:single "false"]
+         [:answernumbering "abc"]
          (common-part)
          (for [x answers]
-          [:answer {:fraction "0"} {:format "html"}
-            [:text (cdata x)]
-            [:feedback {:format "html"} [:text]]] )]))
+          [:answer {:fraction (second x) :format "html"}
+            [:text (cdata (first x))]
+            [:feedback {:format "html"} [:text]]])]))
 
 (defn matching-sets "Match the corresponding set expressions!"
   [n filename]
@@ -60,11 +60,29 @@
     (spit filename (category "halmaz/level3") :append true)
     (loop [i 0]
       (when (< i n)
-        (let [[q a id ft ff] (sets/set-matching-question i)]
-          (spit filename (matching-questions id q a ft ff) :append true))
-          (recur (inc i))  ))
+        (let [[q a id ft] (sets/set-matching-question i)]
+          (spit filename (matching-questions id q a ft) :append true))
+        (recur (inc i))))
+    (spit filename "</quiz>" :append true)))
+
+(defn members-of-sets "Select the suitable elements!"
+  [n filename]
+  (do
+    (spit filename "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<quiz>")
+    (spit filename (category "halmaz/level1") :append true)
+    (loop [i 0]
+      (when (< i n)
+        (let [all (sets/construct-set i)]
+          (if (nil? all)
+            (recur i)
+            (let [[q a id fb] all]
+              (spit filename (multi id q fb a) :append true)
+              (recur (inc i)))))))
     (spit filename "</quiz>" :append true)))
 
 
 (defn -main []
   (println "Use the REPL to generate questions!"))
+
+(comment (members-of-sets 5 "HM1a.xml")) ; set construction
+(comment (matching-sets 100 "HP3a.xml")) ; set pairing
--- src/logic/set.clj
+++ src/logic/set.clj
@@ -1,7 +1,8 @@
 (ns logic.set
   (:require [clojure.string :as s]
             [clojure.java.io :as io]
-            [base64-clj.core :as b64])
+            [base64-clj.core :as b64]
+            [clojure.set :as cs])
   (:gen-class))
 
 (defn combine "combine the elements of 2 sets with all operators"
@@ -36,6 +37,9 @@
      (combine f2 f0)
      (my-complement f2)))
 
+;; for MCQ questions
+(def prize   "if you have good answer" [0.0 100.0 50.0 33.33333 25.0])
+(def penalty "if you haven't" [0.0 -33.33333 -50.0 -100.0 0.0]) 
 
 (def code-uni "the key of the universe (the biggest set)" 255)
 ;; alternative
@@ -49,8 +53,8 @@
 (defn keys-and-set-of-sets "list of (keys and list of corresponding sets)"
   [sets]
   (for [key (range 0 code-uni)
-    :let [s (set-filter key sets)]
-    :when (and (seq s) (seq (rest s)))]
+        :let [s (set-filter key sets)]
+        :when (and (seq s) (seq (rest s)))]
     [key s]))
 
 (def all-sets "all interesting sets grouped by key"
@@ -86,6 +90,32 @@
             :symdiff (bit-xor k1 k2)))))
     (case e  :a setA, :b setB, :c setC :0 set0)))
 
+;; generate sets for easy calculations
+
+(def U "Universe for our set-world" (set (range 1 11)))
+
+(defn generate-set "generate a subset of U"
+  []
+  (set (take 5 (shuffle (range 1 11)))))
+
+(defn set-value "calculate the value of an expression"
+  [e a b c]
+  (if (seq? e)
+    (let [op (first e)
+          k1 (set-value (second e) a b c)]
+      (if (= :comp op)
+        (cs/difference U k1)
+        (let [k2 (set-value (nth e 2) a b c)]
+          (case op
+            :isect   (cs/intersection k1 k2)
+            :union   (cs/union k1 k2)
+            :diff    (cs/difference k1 k2)
+            :symdiff (cs/union
+                       (cs/difference k1 k2)
+                       (cs/difference k2 k1))))))
+    (case e :a a :b b :c c :0 #{})))
+
+
 (defn write "MathJax compatible string of an expression"
   ([e] (write e false))
   ([e outer]
@@ -94,22 +124,21 @@
        (if (= :comp op)
          (str "\\overline{" (write (second e) false) "}")
          (let [wop (case op :isect "\\cap " :union "\\cup "
-                   :diff "\\backslash " :symdiff "\\Delta ")
+                    :diff "\\backslash " :symdiff "\\Delta ")
                w1 (write (second e) true)
                w2 (write (nth e 2) true)]
           (if outer
             (str "(" w1 wop w2 ")")
             (str w1 wop w2)))))
-   (case e :a "A" :b "B" :c "C" :0 "\\emptyset"))))
+    (case e :a "A" :b "B" :c "C" :0 "\\emptyset"))))
 
-(defn question-expressions "list of expressions"
-  [ep]
+(defn question-expressions "list of expressions" [ep]
     (map #(str "<li>\\(" (write (second %)) "\\)</li>") ep))
 
 (defn generate-question "complete question" [ep]
   (str "<p>Adottak az alábbi halmazok:</p><ol>"
     (s/join (question-expressions ep)) ; clojure.string used here
-      "</ol><p>az alább felsorolt halmazok esetén adja meg,"
+    "</ol><p>az alább felsorolt halmazok esetén adja meg,"
       " hogy azok ezek közül melyikkel esnek egybe!</p>"))
 
 (defn generate-answers "the matching expressions" [ep]
@@ -118,18 +147,28 @@
 (defn picture-data "content of the picture" [key]
   (b64/encode (slurp (io/resource (str "s3-" key ".png")))))
 
+(defn colored "the region coded with i is colored" [i a]
+  (if (pos? (bit-and i (first a))) "X" "-"))
+
 (defn feedback-text "generate the text of it" [ep]
-  ; the expressions and pictures
-  (map #(str "<p>\\("
-             (write (second %)) ; exp. from question
-             "\\) &ndash; \\("
-             (write (nth % 2)) ; exp. from answer
-             "\\)</p>"
-             "<img src=\"@@PLUGINFILE@@/s3-"
-             (first %)
-             ".png\" alt=\"\" role=\"presentation\" "
-             "style=\"vertical-align:text-bottom; margin: 0 .5em;\" "
-             "class=\"img-responsive\" height=\"240\" width=\"290\"><br>") ep))
+  ; a table to describe the solution
+  (map #(str "<p>\\(" (write (second %)) "\\) és \\(" (write (nth % 2))
+             "\\)</p><br><table align=\"center\" border=\"1\"><tbody><tr>"
+             "<td>-A</td><td>-A</td><td>-A</td><td>-A</td>"
+             "<td>+A</td><td>+A</td><td>+A</td><td>+A</td></tr>"
+             "<tr><td>-B</td><td>-B</td><td>+B</td><td>+B</td>"
+             "<td>-B</td><td>-B</td><td>+B</td><td>+B</td></tr>"
+             "<tr><td>-C</td><td>+C</td><td>-C</td><td>+C</td>"
+             "<td>-C</td><td>+C</td><td>-C</td><td>+C</td></tr><tr>"
+             "<td>" (colored 128 %) "</td>"
+             "<td>" (colored 16 %) "</td>"
+             "<td>" (colored 32 %) "</td>"
+             "<td>" (colored 64 %) "</td>"
+             "<td>" (colored 1 %) "</td>"
+             "<td>" (colored 2 %) "</td>"
+             "<td>" (colored 4 %) "</td>"
+             "<td>" (colored 8 %) "</td>"
+             "</tbody></table>") ep))
 
 (defn feedback-files "the referenced files" [ep]
   (for [x ep]
@@ -142,7 +181,55 @@
   (let [ep (expression-pairs)
         question (generate-question ep)
         answers (generate-answers ep)
-        id (str "HP1-" i)
-        ft (s/join (feedback-text ep))
-        ff (s/join (feedback-files ep))]
-       `(~question ~answers ~id ~ft ~ff)))
+        id (str "HP3-" i)
+        ft (s/join (feedback-text ep))]
+       `(~question ~answers ~id ~ft)))
+
+(defn construct-set-question [sA sB sC e]
+  (str "Ha adott az \\(A=\\{" 
+       (s/join ", " (apply sorted-set sA))
+       "\\}\\), \\(B=\\{"
+       (s/join ", " (apply sorted-set sB))
+       "\\}\\) és \\(C=\\{"
+       (s/join ", " (apply sorted-set sC))
+       "\\}\\) halmazok, akkor a \\("
+       (write e)
+       "\\) halmaznak az alábbiak közül melyek az elemei?"))
+
+(defn construct-set-feedback [sol]
+  (str "<p>A művelet eredménye: \\(\\{" 
+       (s/join ", " (apply sorted-set sol))
+       "\\}\\)</p>"))
+
+(defn number-of-good-answers [g]
+  (case g
+    1 1
+    2 (+ 1 (rand-int 2))
+    3 (+ 1 (rand-int 3))
+    (4 5 6 7) (+ 1 (rand-int 4))
+    8 (+ 2 (rand-int 3))
+    9 (+ 3 (rand-int 2))
+    10 4))
+
+(defn construct-set-answer [sol no-sol]
+  (let [sz (count sol)
+        r  (number-of-good-answers sz)
+        s4 (take r sol)
+        n3 (take (- 4 r) no-sol)]
+    (concat 
+      (for [x s4] [x (nth prize r)])
+      (for [y n3] [y (nth penalty r)]))))
+
+(defn construct-set "Generate the base sets and their combination." [id]
+  (let [sA (generate-set)
+        sB (generate-set)
+        sC (generate-set)
+        e  (rand-nth f3)
+        sol (set-value e sA sB sC)          ; the resulting set
+        no-sol (cs/difference U sol)]       ; other elements
+    (when-not (empty? sol)
+      (let [q (construct-set-question sA sB sC e)
+            a (construct-set-answer sol no-sol)
+            fb (construct-set-feedback sol)
+            i (str "HM1-" (inc id))]
+       [q a i fb]))))
